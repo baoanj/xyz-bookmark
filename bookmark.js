99e58980c5a29b3ca91309658732c0a0d9359a0c
baoanjie <baoanj@126.com>
2018-12-05 15:59:55 +0800

    优化布局和字体

--- bookmark.js
+++ bookmark.js
@@ -17,17 +17,26 @@ window.onload = () => {
       if (e.target.nodeName.toUpperCase() === 'P') {
         for (let i = 0; i < bookmarkBar.length; i += 1) {
           if (bookmarkBar[i].id === e.target.getAttribute('data-id')) {
-            let moreHTML = '';
-            bookmarkBar[i].children.forEach(item => {
+            let leftMoreHTML = '', rightMoreHTML = '';
+            const mid = Math.max(Math.ceil(bookmarkBar[i].children.length / 2), 28);
+            bookmarkBar[i].children.forEach((item, index) => {
               if (item.children) {
-                moreHTML += `<p>${item.title}</p>`
+                if (index < mid) leftMoreHTML += `<p>${item.title}</p>`;
+                else rightMoreHTML += `<p>${item.title}</p>`;
               } else {
-                moreHTML += `<p><img src="chrome://favicon/size/8@2x/${item.url}">
+                if (index < mid) {
+                  leftMoreHTML += `<p><img src="chrome://favicon/size/8@2x/${item.url}">
                   <a title="${item.title}\n${item.url}" href="${item.url}"
-                  target="_blank">${item.title}</a></p>`
+                  target="_blank">${item.title}</a></p>`;
+                } else {
+                  rightMoreHTML += `<p><img src="chrome://favicon/size/8@2x/${item.url}">
+                  <a title="${item.title}\n${item.url}" href="${item.url}"
+                  target="_blank">${item.title}</a></p>`;
+                }
               }
             });
-            document.getElementById('bookmark-more').innerHTML = moreHTML;
+            document.getElementById('bookmark-more-left').innerHTML = leftMoreHTML;
+            document.getElementById('bookmark-more-right').innerHTML = rightMoreHTML;
           }
         }
       }
