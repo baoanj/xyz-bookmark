658adbae8c0dd6e0346affb03efa6634a3710414
baoanjie <baoanj@126.com>
2019-02-13 17:36:53 +0800

    优化UI

--- bookmark.js
+++ bookmark.js
@@ -1,12 +1,13 @@
 window.onload = () => {
   chrome.bookmarks.getTree(data => {
     const bookmarkBar = data[0].children[0].children; // 书签栏
+    const favicon = 'chrome://favicon/size/8@2x/';
     let barHTML = '<ul>';
     bookmarkBar.forEach(item => {
       if (item.children) {
         barHTML += `<li><p data-id="${item.id}">${item.title}</p></li>`;
       } else {
-        barHTML += `<li><img src="chrome://favicon/size/8@2x/${item.url}">
+        barHTML += `<li><img src="${favicon}${item.url}">
           <a title="${item.title}\n${item.url}" href="${item.url}"
           target="_blank">${item.title}</a></li>`;
       }
@@ -18,18 +19,18 @@ window.onload = () => {
         for (let i = 0; i < bookmarkBar.length; i += 1) {
           if (bookmarkBar[i].id === e.target.getAttribute('data-id')) {
             let leftMoreHTML = '', rightMoreHTML = '';
-            const mid = Math.max(Math.ceil(bookmarkBar[i].children.length / 2), 28);
+            const mid = Math.max(Math.ceil(bookmarkBar[i].children.length / 2), 27);
             bookmarkBar[i].children.forEach((item, index) => {
               if (item.children) {
                 if (index < mid) leftMoreHTML += `<p>${item.title}</p>`;
                 else rightMoreHTML += `<p>${item.title}</p>`;
               } else {
                 if (index < mid) {
-                  leftMoreHTML += `<p><img src="chrome://favicon/size/8@2x/${item.url}">
+                  leftMoreHTML += `<p><img src="${favicon}${item.url}">
                   <a title="${item.title}\n${item.url}" href="${item.url}"
                   target="_blank">${item.title}</a></p>`;
                 } else {
-                  rightMoreHTML += `<p><img src="chrome://favicon/size/8@2x/${item.url}">
+                  rightMoreHTML += `<p><img src="${favicon}${item.url}">
                   <a title="${item.title}\n${item.url}" href="${item.url}"
                   target="_blank">${item.title}</a></p>`;
                 }
